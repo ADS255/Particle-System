86e8e207f9c7b5febf96c1b2b1f65a5cfaa68221
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2025-05-05 20:28:02 +0100

    feature: added profiler/

--- src/benchmarking/PerformanceProfiler.cpp
+++ src/benchmarking/PerformanceProfiler.cpp
@@ -13,10 +13,19 @@ void PerformanceProfiler::BenchMark(double frameTime)
 {
 	currentFrameData.frameTime = frameTime * 1000.0f;
 
+	double frameTimeMs = static_cast<double>(frameTime * 1000.0);
+	frameTimeHistory.push_back(frameTimeMs);
+	totalFrameTime += frameTimeMs;
+
+	if (frameTimeHistory.size() > MaxSamples) {
+		totalFrameTime -= frameTimeHistory.front();
+		frameTimeHistory.pop_front();
+	}
+
+	averageFrameTimeMs = totalFrameTime / frameTimeHistory.size();
+
 	if (particleSystem)
 	{
-		currentFrameData.updateTime = particleSystem->GetUpdateTime();
-		currentFrameData.renderTime = particleSystem->GetRenderTime();
 		currentFrameData.activeParticleCount = particleSystem->GetActiveParticleCount();
 		currentFrameData.particleGPUSizeBytes = particleSystem->GetParticleGPUSizeBytes();
 		currentFrameData.totalParticlesGPUSizeBytes = particleSystem->GetTotalParticlesGPUSizeBytes();
@@ -24,10 +33,17 @@ void PerformanceProfiler::BenchMark(double frameTime)
 		currentFrameData.drawCallsPerFrame = particleSystem->GetTotalDrawCalls();
 	}
 
-	if (recordData && currentFrame < maxCaptureFrames)
+	if (recordData && particleSystem && particleSystem->activeParticleCount > 0)
 	{
-		data[currentFrame] = currentFrameData;
+		data.push_back(currentFrameData);
 		currentFrame++;
+
+		if (averageFrameTimeMs >= 16.6f)
+		{
+			recordData = false;
+			ExportDataCSV("D:/Main/University/KV6013 Computing Project/Data/benchmark_data.csv");
+			data.clear();
+		}
 	}
 }
 
@@ -42,6 +58,41 @@ void PerformanceProfiler::Display()
 	ImGui::Text("Total Particles GPU Size: %u bytes", currentFrameData.totalParticlesGPUSizeBytes);
 	ImGui::Text("Data Transfer Per Update: %u bytes", currentFrameData.totalDataTransferPerUpdateBytes);
 	ImGui::Text("Draw Calls Per Frame: %u", currentFrameData.drawCallsPerFrame);
+	ImGui::Text("Average Frametime: %.3f", averageFrameTimeMs);
+
+	ImGui::Separator();
+	ImGui::Text("Frames Measured: %u", currentFrame);
+	if (ImGui::Button("Record Metrics"))
+	{
+		recordData = !recordData;
+	}
 
 	ImGui::End();
+}
+
+void PerformanceProfiler::ExportDataCSV(const std::string& filename)
+{
+	std::ofstream file(filename, std::ios::out);
+
+	if (!file) {
+		std::cerr << "Error creating/opening file: " << filename << std::endl;
+		return;
+	}
+
+	file << "FrameTime,ActiveParticleCount,TotalParticlesGPUSizeBytes,TotalDataTransferPerUpdateBytes,DrawCallsPerFrame\n";
+
+	for (const auto& metrics : data) {
+
+		if (metrics.activeParticleCount > 0)
+		{
+			file << metrics.frameTime << ","
+				<< metrics.activeParticleCount << ","
+				<< metrics.totalParticlesGPUSizeBytes << ","
+				<< metrics.totalDataTransferPerUpdateBytes << ","
+				<< metrics.drawCallsPerFrame << "\n";
+		}
+	}
+
+	file.close();
+	std::cout << "CSV file saved successfully: " << filename << std::endl;
 }
--- src/benchmarking/PerformanceProfiler.h
+++ src/benchmarking/PerformanceProfiler.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <deque>
+#include <string>
+
 #include "PerformanceMetrics.h"
 #include "ParticleEmitter.h"
 
@@ -12,13 +18,19 @@ public:
 	void BenchMark(double frameTime);
 	void Display();
 
+	void ExportDataCSV(const std::string& filename);
+
 private:
 
 	ParticleEmitter* particleSystem;
 	PerformanceMetrics currentFrameData = {};
 	bool recordData = false;
 
-	const unsigned int maxCaptureFrames = 1000;
-	std::vector<PerformanceMetrics> data = std::vector<PerformanceMetrics>(maxCaptureFrames);
+	std::vector<PerformanceMetrics> data = std::vector<PerformanceMetrics>(100000);
 	unsigned int currentFrame = 0;
+
+	static constexpr int MaxSamples = 100;
+	std::deque<double> frameTimeHistory;
+	double totalFrameTime = 0.0f;
+	double averageFrameTimeMs = 0.0f;
 };
